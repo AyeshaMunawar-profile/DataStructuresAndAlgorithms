5c16dca96ea440d65511ee9810ae21945b81c643
Ayesha <ayeshamunawarofficial@gmail.com>
2020-06-15 17:42:35 +0500

    Linked-List> Reverese a string using doubly linked list
    Linked-List> Reverese a string using doubly linked list
    
    trave ther list from tail to the head

--- Linked List/Reverse-a-string-using-linked-list.js
+++ Linked List/Reverse-a-string-using-linked-list.js
@@ -1,9 +1,7 @@
-const fruits = ['Apples', 'Grapes', 'Pears', 'Banana', 'Nutella'];
-const prices = [1000, 2000, 4000, 200, 500];
+const myString = ['a', 'b', 'c', 'd', 'e'];
 
 class node {
-    constructor(key, value, nextPtr, prevPtr) {
-        this.key = key;
+    constructor(value, nextPtr, prevPtr) {
         this.value = value;
         this.nextPtr = nextPtr;
         this.prevPtr = prevPtr;
@@ -35,65 +33,66 @@ class linkedList {
         return currentNode;
     }
 
-    populateLinkedList(keys, values) { // fill the linked list with keys and value arrays fill corresponding keys and value in node
-        if (keys.length === values.length) {
-            for (let i = 0; i < keys.length; i++) {
-                this.pushNode(keys[i], values[i]);
+    populateLinkedList(values) { // fill the linked list with  value arrays
+        if (values.length) {
+            for (let i = 0; i < values.length; i++) {
+                this.pushNode(values[i]);
             }
         } else {
-            console.log("Data provided is not correct i.e send keys and values array of equal length !");
+            console.log("Data provided is not correct i.e values array of valid length !");
         }
     }
 
-    populateLinkedListReverse(keys, values) {
-        if (keys.length === values.length) {
-            for (let i = 0; i < keys.length; i++) {
-                this.shiftNode(keys[i], values[i]);
+    populateLinkedListReverse(values) {
+        if (values.length) {
+            for (let i = 0; i < values.length; i++) {
+                this.shiftNode(values[i]);
             }
         } else {
-            console.log("Data provided is not correct i.e send keys and values array of equal length !");
+            console.log("Data provided is not correct i.e send values array of valid length !");
         }
     }
 
 
     printLinkedList() {
+        let finalString = "";
         if (!this.head && !this.tail) { // empty linked list
             console.log("Empty Linked List");
-        } else { // start from the head og the linked list and print key and values till the last node i.e. tail node
+        } else { // start from the head og the linked list and print values till the last node i.e. tail node
 
             let currentNode = this.head;
             let i = 0;
             console.log("----------------------------------------------------------------\n");
             while (currentNode) {// traverse list from head to tail node
-                console.log("Node " + i + "\n key:" + currentNode.key + "\n value:" + currentNode.value + "\n");
+                console.log(currentNode.value);
+                finalString += currentNode.value;
                 currentNode = currentNode.nextPtr;
                 i++;
             }
-
-            console.log("Summary: \n " + "length of linked list: " + this.size + "\n Head node: \n   " + this.head.key + "\n   " + this.head.value + "\n Tail node: \n   " + this.tail.key + "\n   " + this.tail.value + "\n----------------------------------------------------------------");
+            console.log("----------------------------------------------------------------\n");
         }
     }
 
     printLinkedListReversed() {
         if (!this.head && !this.tail) { // empty linked list
             console.log("Empty Linked List");
-        } else { // start from the head og the linked list and print key and values till the last node i.e. tail node
+        } else { // start from the head og the linked list and print values till the last node i.e. tail node
 
             let currentNode = this.tail;
             let i = this.size - 1;
             console.log("----------------------------------------------------------------\n");
             while (currentNode) {// traverse list from head to tail node
-                console.log("Node " + i + "\n key:" + currentNode.key + "\n value:" + currentNode.value + "\n");
+                console.log("Node " + i + "\n value:" + currentNode.value + "\n");
                 currentNode = currentNode.prevPtr;
                 i--;
             }
 
-            console.log("Summary: \n " + "length of linked list: " + this.size + "\n Head node: \n   " + this.head.key + "\n   " + this.head.value + "\n Tail node: \n   " + this.tail.key + "\n   " + this.tail.value + "\n----------------------------------------------------------------");
+            console.log("Summary: \n " + "length of linked list: " + this.size + "\n Head node: \n   " + this.head.value + "\n Tail node: \n   " + this.tail.value + "\n----------------------------------------------------------------");
         }
     }
 
-    pushNode(key, value) { // add node at the end of the linked list
-        let newNode = new node(key, value, null, null);
+    pushNode(value) { // add node at the end of the linked list
+        let newNode = new node(value, null, null);
         if (!this.tail && !this.head) { // empty linked list
             this.head = newNode;
             this.tail = newNode;
@@ -106,8 +105,8 @@ class linkedList {
         }
     }
 
-    unshiftNode(key, value) { // add node at the begining of the linked list
-        let newNode = new node(key, value, null, null);
+    unshiftNode(value) { // add node at the begining of the linked list
+        let newNode = new node(value, null, null);
         if (!this.tail && !this.head) { // empty linked list
             this.head = newNode;
             this.tail = newNode;
@@ -152,23 +151,23 @@ class linkedList {
         }
     }
 
-    insertAtIndex(index, key, value) { // insert from index 0 to size i.e. insert after given index
-        if (this.isInputInValid(index) || this.isInputInValid(key) || this.isInputInValid(value)) { // input validation
-            console.log("Please enter a correct index i.e. between 0 and " + this.size + " \n check if key and value entered are correct");
+    insertAtIndex(index, value) { // insert from index 0 to size i.e. insert after given index
+        if (this.isInputInValid(index) || this.isInputInValid(value)) { // input validation
+            console.log("Please enter a correct index i.e. between 0 and " + this.size + " \n check if value entered are correct");
             return false;
         } else {
             if (index > this.size) {
                 console.log("cannot insert the new node bacause the index is greater than the size of the linked list ");
                 return false;
             } else if (index === this.size - 1) { // insert after last node
-                this.pushNode(key, value);
+                this.pushNode(value);
                 return true;
             } else if (index === 0) { // insert at the beginning of the linked list
-                this.unshiftNode(key, value);
+                this.unshiftNode(value);
             } else if (index < this.size - 1 && index > 0) { // insert at any point other than after tail and before head
                 let currentNode = this.head;
                 currentNode = this.traverseToIndex(index - 1); // traverse to the node behind the node at given index
-                let newNode = new node(key, value, currentNode.nextPtr, currentNode);
+                let newNode = new node(value, currentNode.nextPtr, currentNode);
                 currentNode.nextPtr.prevPtr = newNode;
                 currentNode.nextPtr = newNode;
                 this.size++;
@@ -182,7 +181,7 @@ class linkedList {
 
     deleteAtIndex(index) { // delete from index 0 to tail
         if (this.isInputInValid(index)) {
-            console.log("Please enter a correct index i.e between 0 and " + this.size - 1 + " \n check if key and value entered are correct");
+            console.log("Please enter a correct index i.e between 0 and " + this.size - 1 + " \n check if  value entered are correct");
             return false;
         } else if (index > this.size - 1) {
             console.log("Cannot delete the node as the index is greater than the size of the linked list");
@@ -206,10 +205,10 @@ class linkedList {
         }
     }
 
-    lookUp(key) {
+    lookUp(value) {
         let currentNode = this.head;
         while (currentNode) {
-            if (!(currentNode.key.localeCompare(key))) { // if the given key and the current node's key is equal
+            if (!(currentNode.value.localeCompare(value))) { // if the given value and the current node's value is equal
                 this.printNode(currentNode);
                 return true;
             }
@@ -217,31 +216,36 @@ class linkedList {
             currentNode = currentNode.nextPtr;
 
         }
-        console.log("The node with the given key i.e. " + key + " not found ");
+        console.log("The node with the given value i.e. " + value + " not found ");
         return false;
     }
 
 
     printNode(node, index) {
         if (index) {
-            console.log("Node " + index + "\n key:" + node.key + "\n value:" + node.value + "\n");
+            console.log("Node " + index + "\n value:" + node.value + "\n");
         } else {
-            console.log("key:" + node.key + "\n value:" + node.value + "\n");
+            console.log("value:" + node.value + "\n");
         }
+    }
+
+    getListValuesReversedInAnArray() {
+        let currentNode = this.tail;
+        let index = this.size - 1;
+        let reversedString = "";
+        while (currentNode && index >= 0) {
+            reversedString += currentNode.value;
+            currentNode = currentNode.prevPtr;
+            index--;
+        }
+        console.log("Reversed string is : " + reversedString);
+        return reversedString;
     }
 }
 
 
 let myLinkedList = new linkedList();
-myLinkedList.populateLinkedList(fruits, prices); // fill linked list with corresponding keys and values arrays
+myLinkedList.populateLinkedList(myString);
 myLinkedList.printLinkedList();
-myLinkedList.insertAtIndex(2, "cream", 2765);
-myLinkedList.printLinkedList();
-myLinkedList.deleteAtIndex(3);
-myLinkedList.printLinkedList();
-myLinkedList.lookUp("Nutella");
-// myLinkedList.printLinkedListReversed();
-// myLinkedList.unshiftNode("cream", 875648);
-// myLinkedList.printLinkedList();
-// myLinkedList.shiftNode();
-// myLinkedList.printLinkedList();
+myLinkedList.getListValuesReversedInAnArray();
+
